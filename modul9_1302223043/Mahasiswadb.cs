a881cd930fa8702270d8dd0b6c2780f7d8b0235f
cruzte4 <162765665+cruzte4@users.noreply.github.com>
2024-05-04 09:08:02 +0700

    Add files via upload

---
+++ modul9_1302223043/Mahasiswadb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.mahasiswa;
+
+namespace modul9_1302223043
+{
+    public class Mahasiswadb : DbContext
+    {
+        public Mahasiswadb(DbContextOptions<Mahasiswadb> options)
+        : base(options) { }
+        public DbSet<Mahasiswa> mhs => Set<Mahasiswa>();
+    }
+}
